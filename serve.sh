d73f3b5697fbedceb62cfd8f079d958a689f7239
Dave Thwaites <github@dave.thwaites.org.uk>
2020-05-13 22:20:20 +0100

    Run Jekyll with --safe
    Using the --safe parameter better mimics the behaviour of GitHub Pages

--- serve.sh
+++ serve.sh
@@ -4,4 +4,4 @@ PORT=${1:-4000}
 
 # pipe everything (inc. stderr) through grep, to filter out annoying messages about
 # the _sass folder...
-bundle exec jekyll serve --host $(hostname -I) --port $PORT 2>&1 | grep -v "Invalid theme folder: _sass"
+bundle exec jekyll serve --host $(hostname -I) --port $PORT --safe 2>&1 | grep -v "Invalid theme folder: _sass"
