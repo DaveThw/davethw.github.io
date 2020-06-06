9895fb8074af5b03bccc4f418372c4f421b3893e
Dave Thwaites <github@dave.thwaites.org.uk>
2020-06-06 21:10:23 +0100

    Add warning for missing .env file

--- serve.sh
+++ serve.sh
@@ -2,6 +2,11 @@
 
 PORT=${1:-4000}
 
+if [[ ! -e .env ]]; then
+  echo -e "\e[33mWarning: .env file not found - GitHub Metadata gem won't be able to authenticate\e[0m"
+  echo
+fi
+
 # pipe everything (inc. stderr) through grep, to filter out annoying messages about
 # the _sass folder...
 bundle exec jekyll serve --host $(hostname -I) --port $PORT --safe 2>&1 | grep -v "Invalid theme folder: _sass"
