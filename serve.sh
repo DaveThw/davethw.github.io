d7dca1281fbad3816cf23b221ce4ec4c5d416d47
Dave Thwaites <github@dave.thwaites.org.uk>
2020-06-08 09:02:52 +0100

    Better help for missing .env file

--- serve.sh
+++ serve.sh
@@ -4,7 +4,16 @@ PORT=${1:-4000}
 
 if [[ ! -e .env ]]; then
   echo -e "\e[33mWarning: .env file not found - GitHub Metadata gem won't be able to authenticate\e[0m"
-  echo
+  cat <<- EOF
+
+	.env should contain somthing like:
+	# GitHub Access Token for Jekyll (for github-metadata)
+	JEKYLL_GITHUB_TOKEN=<Access Token from GitHub>
+
+	To create an Access Token, see:
+	https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
+
+EOF
 fi
 
 # pipe everything (inc. stderr) through grep, to filter out annoying messages about
